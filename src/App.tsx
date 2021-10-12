4cb0b0ba393b4b132b85f81257edaae9afe51cab
Luke Halko <halko@udel.edu>
2021-10-12 14:47:00 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    Edit <code>src/App.tsx</code> anHGKYJGHFhjgjfJGFHDJYTHd save to reload.
                 </p>
             </header>
         </div>
