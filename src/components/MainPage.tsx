b1608e88a6b346f5412a00f5d2737bbd2fe746e7
cthacker-udel <cthacker@udel.edu>
2021-10-14 13:35:53 -0400

    update2

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -4,32 +4,32 @@ import { SemesterTable } from './SemesterTable';
 import React from 'react';
 
 export const MainPage = (): JSX.Element => {
-	
-	return(
-		<Container>
-			<Row>
-				<Col>
-					
-				</Col>
-				<Col>
-					<Row>
-						<Col>
-							{/* concentration list */}
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							{/* reqs */}
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<SemesterTable />
-				</Col>
-			</Row>
-		</Container>
-	);
+    
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            {/* concentration list */}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            {/* reqs */}
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <SemesterTable />
+                </Col>
+            </Row>
+        </Container>
+    );
 
 };
