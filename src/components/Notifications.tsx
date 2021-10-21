f2f90d8906a796bf91a3e2e21c7a9e5b5cd4954f
cthacker-udel <cthacker@udel.edu>
2021-10-21 04:20:57 -0400

    Fixed error #1

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -6,7 +6,7 @@ export const WelcomeToast = (props: {display: boolean }): JSX.Element =>
         <Modal show={props.display}>
             <Modal.Header>
                 <Modal.Title>
-                    Welcome to the Course Scheduler!
+                    Welcome to the Course App!
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>Here you can drag and drop courses into your schedule, and also concentrations are available to select from the dropdown as well!</Modal.Body>
