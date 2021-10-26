5874542e140eff881f35ac62649295800dd945b8
lukehalko <halko@udel.edu>
2021-10-26 10:01:00 -0400

    updated Concentration interface

--- src/interfaces/concentration.ts
+++ src/interfaces/concentration.ts
@@ -1,3 +1,19 @@
+import { Course as CourseType } from "../interfaces/course";
+
 export interface Concentration{
-    name:string
+    name:string,
+    core:CourseType[],
+    capstone:CourseType[],
+    lab:CourseType[][],
+    writing: CourseType[],
+    conc:{
+        general:CourseType[],
+        stats:CourseType[],
+        systems:CourseType[],
+        elective:CourseType[],
+        ochem:CourseType[],
+        data:CourseType[],
+        cybersecurity:CourseType[],
+        track:CourseType[]
+    }
 }
