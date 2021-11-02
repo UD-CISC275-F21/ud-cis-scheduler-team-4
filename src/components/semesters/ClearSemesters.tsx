81f2d4aedb4f4a050e77824652e501522057360d
lukehalko <halko@udel.edu>
2021-11-01 21:16:05 -0400

    initialized ClearSemesters.tsx file

---
+++ src/components/semesters/ClearSemesters.tsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+import React from "react";
+import { SemesterType } from "../../interfaces/semester";
+
+export const ClearSemesters = (props:{ setSemesterCourses: React.Dispatch<React.SetStateAction<SemesterType[]>>}): JSX.Element =>{
+    return (
+        <Button onClick = {()=>{
+            props.setSemesterCourses([]);
+        }
+        }>
+            Clear
+        </Button>
+    );
+};
