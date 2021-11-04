4bb0a4311520312874c6c682cbabca25e11ae0f5
Cameron Thacker <cthacker@udel.edu>
2021-11-04 01:17:20 -0400

    Added button to delete semester

--- src/components/semesters/SemesterTable.tsx
+++ src/components/semesters/SemesterTable.tsx
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { Semester } from "./Semester";
 import { SemesterType } from "../../interfaces/semester";
-import { Accordion } from "react-bootstrap";
 
 /*
 
@@ -17,14 +16,7 @@ export const SemesterTable = (props: { semesters: number, semestersCourses: Seme
         <>
             {
                 new Array(props.semesters).fill(0).map((elem, ind) =>
-                    <Accordion key={`accordion ${ind}`} defaultActiveKey="0">
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header>{`Semester ${ind+1}`}</Accordion.Header>
-                            <Accordion.Body>
-                                <Semester ind={ind} key={`semester-table-key-${ind}`} semesterCourses={props.semestersCourses} setSemesterCourses={props.setSemesterCourses} />
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
+                    <Semester ind={ind} key={`semester-table-key-${ind}`} semesterCourses={props.semestersCourses} setSemesterCourses={props.setSemesterCourses} />
                 )
             }
         </>
