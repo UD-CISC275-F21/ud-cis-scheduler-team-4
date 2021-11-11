18e048b96539734b887b514b1c2ca2122ccac93b
Cameron Thacker <cthacker@udel.edu>
2021-11-11 03:36:02 -0500

    Added some tests -- the useful links rendering is not working although it should be

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -57,17 +57,17 @@ export const MainPage = (): JSX.Element => {
                         </Col>
                     </Row>
                     <Row>
-                        <Navbar bg="light" expand="lg">
+                        <Navbar bg="light" expand="lg" data-testid="navbar">
                             <Container>
                                 <Navbar.Brand href="#home">UDCIS Course Scheduler</Navbar.Brand>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="me-auto">
-                                        <NavDropdown title="Useful Links" id="basic-navbar-nav">
-                                            <NavDropdown.Item href="https://udapps.nss.udel.edu/CoursesSearch/">Course Search</NavDropdown.Item>
-                                            <NavDropdown.Item href="https://www.cis.udel.edu/academics/undergraduate-programs/resources/courses/">CISC Undergraduate Courses</NavDropdown.Item>
-                                            <NavDropdown.Item href="https://webreg.nss.udel.edu/registration/schedule/">Registration Add/Drop</NavDropdown.Item>
-                                            <NavDropdown.Item href="https://ud-cis-teaching.github.io/student-guidance/">UD CIS Student Guidance</NavDropdown.Item>
+                                        <NavDropdown title="Useful Links" id="basic-navbar-nav" data-testid="navbardropdown">
+                                            <NavDropdown.Item href="https://udapps.nss.udel.edu/CoursesSearch/" data-testid="navdropdownitem1">Course Search</NavDropdown.Item>
+                                            <NavDropdown.Item href="https://www.cis.udel.edu/academics/undergraduate-programs/resources/courses/" data-testid="navdropdownitem2">CISC Undergraduate Courses</NavDropdown.Item>
+                                            <NavDropdown.Item href="https://webreg.nss.udel.edu/registration/schedule/" data-testid="navdropdownitem3">Registration Add/Drop</NavDropdown.Item>
+                                            <NavDropdown.Item href="https://ud-cis-teaching.github.io/student-guidance/" data-testid="navdropdownitem4">UD CIS Student Guidance</NavDropdown.Item>
                                         </NavDropdown>
                                         <DropdownMenu setConcentration={setConcentration} semesterCourses={semesterCourses} setSemesterCourses={setSemesterCourses}></DropdownMenu>
                                         <AddSemesterButton setSemesters={setSemesters} semesters={semesters} />
