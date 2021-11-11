bdfa4e8344ab6b865e969088bf63b219744dd869
cthacker-udel <cthacker@udel.edu>
2021-11-11 18:39:42 -0500

    Refined DisplayCourseList

--- src/components/courses/DisplayCourseList.tsx
+++ src/components/courses/DisplayCourseList.tsx
@@ -6,36 +6,33 @@ import COURSES from "../../json/courses.json";
 import { Course as CourseType } from "../../interfaces/course";
 import { ConcentrationContainerType } from "../../interfaces/concentrationcontainer";
 
-export function DisplayCourseList(props: {concentration: Concentration, setConcentrationContainers: React.Dispatch<React.SetStateAction<ConcentrationContainerType[]>> }): JSX.Element{
+export const DisplayCourseList = (props: {
+        concentration: Concentration;
+        setConcentrationContainers: React.Dispatch<React.SetStateAction<ConcentrationContainerType[]>>;
+    }): JSX.Element => {
 
-    function StringsToCourses(stringCourses: string[]): CourseType[]{
-        /**Takes a list of strings, and returns a list of courses by looking in courses.json for matching names. 
-         * Will need to be optimized to not be O^n, since it currently just loops through the entire json.
+    function StringsToCourses(stringCourses: string[]): CourseType[] {
+        /** Takes a list of strings, and returns a list of courses by looking in courses.json for matching names.
+         *  Will need to be optimized to not be O^n, since it currently just loops through the entire json.
          */
-
         const allCourses = COURSES as CourseType[];
-
-        const tmparr: CourseType[] = stringCourses.map(e => [...allCourses.filter(i => i.name == e)]).flat(2);
-
+        const tmparr: CourseType[] = stringCourses.map(elem => [...allCourses.filter(i => i.name === elem)]).flat(2);
         return tmparr;
-
     }
 
-    if (props.concentration==CONCENTRATIONS[0]){
-        return <AIConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers}></AIConc>;
-    } else if (props.concentration==CONCENTRATIONS[1]){
-        return <BioConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers}></BioConc>;
-    } else if (props.concentration==CONCENTRATIONS[2]){
-        return <SecurityConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} ></SecurityConc>;
-    } else if (props.concentration==CONCENTRATIONS[3]){
-        return <DataScienceConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers}></DataScienceConc>;
-    } else if (props.concentration==CONCENTRATIONS[4]){
-        return <HPCConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers}></HPCConc>;
-    } else if (props.concentration==CONCENTRATIONS[5]){
-        return <NetworksConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers}></NetworksConc>;
-    } else if (props.concentration==CONCENTRATIONS[6]){
-        return <TheoryConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers}></TheoryConc>;
-    } else {
-        return <div></div>;
-    }
-}
+    if (props.concentration === CONCENTRATIONS[0]) {
+        return <AIConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } else if (props.concentration === CONCENTRATIONS[1]) {
+        return <BioConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } else if (props.concentration === CONCENTRATIONS[2]) {
+        return <SecurityConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } else if (props.concentration === CONCENTRATIONS[3]) {
+        return <DataScienceConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } else if (props.concentration === CONCENTRATIONS[4]) {
+        return <HPCConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } else if (props.concentration === CONCENTRATIONS[5]) {
+        return <NetworksConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } else if (props.concentration === CONCENTRATIONS[6]) {
+        return <TheoryConc StringsToCourses={StringsToCourses} setConcentrationContainers={props.setConcentrationContainers} />;
+    } return <div />;
+};
