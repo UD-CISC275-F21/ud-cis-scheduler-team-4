3117fb5b17dceb54371af7e8933ddd449790e932
cthacker-udel <cthacker@udel.edu>
2021-11-11 18:29:58 -0500

    Refined semester

--- src/components/semesters/Semester.tsx
+++ src/components/semesters/Semester.tsx
@@ -27,105 +27,121 @@ export const getSemesterStr = (semesterNum: number): string => {
     }
 };
 
-export const Semester = (props: { ind: number, semesterCourses: SemesterType[], setSemesterCourses: React.Dispatch<React.SetStateAction<SemesterType[]>> }): JSX.Element => {
+export const Semester = (props: { ind: number;
+    semesterCourses: SemesterType[];
+    setSemesterCourses: React.Dispatch<React.SetStateAction<SemesterType[]>>;
+}): JSX.Element => {
     const [courses, setCourses] = useState<CourseType[]>([]);
     const [display, setDisplay] = useState<boolean>(false);
     const [credits, setCredits] = useState<number>(0);
 
-    const func1 = (courses: CourseType[]) => {
-        console.log("---calling func1 with---");
-        courses.forEach(e => console.log(Object.values(e)));
-        setCourses(courses);
-        //getCredits(props.semesterCourses, props.ind);
+    const courseDeleteFunc = (elem: CourseType): string => {
+        const tmpCourses: CourseType[] = [...courses];
+        for (let i = 0; i < tmpCourses.length; i += 1) {
+            const theCourse: CourseType = tmpCourses[i];
+            if (theCourse.name === elem.name) {
+                // found course
+                tmpCourses.splice(i, 1);
+            }
+        }
+        setCourses([...tmpCourses]);
+        const tmpSemesters: SemesterType[] = props.semesterCourses;
+        let tmpSemester: SemesterType = tmpSemesters[0];
+        for (let i = 0; i < tmpSemesters.length; i += 1) {
+            tmpSemester = tmpSemesters[i];
+            if (tmpSemester.semesternum === props.ind + 1) {
+                // found semester
+                tmpSemester = tmpSemesters.splice(i, 1)[0];
+                tmpSemester.courses = [...tmpCourses];
+                tmpSemester.courseSetter([...tmpCourses]);
+                tmpSemesters.splice(i, 0, tmpSemester);
+                props.setSemesterCourses([...tmpSemesters]);
+                return elem.name;
+            }
+        }
+
+        return elem.name;
+    };
+
+    const semesterDeleteFunc = () => {
+
+        const tmpSemesterCourses = [...props.semesterCourses];
+
+        let theSemester = tmpSemesterCourses[0];
+
+        let ind1 = 0;
+
+        for (let i = 0; i < tmpSemesterCourses.length; i += 1) {
+
+            if (tmpSemesterCourses[i].semesternum === props.ind + 1) {
+                // found semester
+                ind1 = i;
+                theSemester = tmpSemesterCourses.splice(i, 1)[0];
+                break;
+            }
+        }
+        theSemester.courses = [];
+        theSemester.courseSetter([]);
+        tmpSemesterCourses.splice(ind1, 0, theSemester);
+        props.setSemesterCourses([...tmpSemesterCourses]);
     };
 
     useEffect(() => {
 
         console.log("rendering semester");
-        if(!props.semesterCourses.find(e => e.semesternum == props.ind+1)){
+        if (!props.semesterCourses.find(elem => elem.semesternum === props.ind + 1)) {
             // not in list
             const semesters: SemesterType[] = [...props.semesterCourses];
-            semesters.push({semesternum: props.ind+1, courses: courses, courseSetter: func1});
+            semesters.push({courseSetter: () => {
+                setCourses(courses);
+            }, courses, semesternum: props.ind + 1 });
             props.setSemesterCourses(semesters);
         }
-    }, []);
-
-    useEffect(() => {
-
-        console.log("----courses are now----");
-        courses.forEach(e => console.log(Object.values(e)));
-
-        // verify that course you are trying to add is not a prereq of course in current semester
-
-
-        getCredits(courses);
-
-    }, [courses]);
+    }, [courses, props]);
 
     const getCredits = (courses: CourseType[]) => {
         console.log("inside getCredits");
         console.log(courses);
         const tmpCourses: CourseType[] = courses;
-        console.log(tmpCourses);
-        let count=0;
-        for(let i=0;i<tmpCourses.length;i++){
-            count+=tmpCourses[i].credits;
+        let count = 0;
+        for (const eachCourse of tmpCourses) {
+            count += eachCourse.credits;
         }
-        console.log("count is: " + count);
         setCredits(count);
     };
-    
 
-    return(
-        <Accordion key={`accordion ${props.ind}`} defaultActiveKey="0">
+    useEffect(() => {
+        console.log("----courses are now----");
+        // verify that course you are trying to add is not a prereq of course in current semester
+        getCredits(courses);
+
+    }, [courses]);
+
+    return (
+        <Accordion defaultActiveKey="0" key={`accordion ${props.ind}`} >
             <Accordion.Item eventKey="0">
                 <Accordion.Header>
                     <Col xs={2}>
-                        {`Semester ${props.ind+1}`}
+                        {`Semester ${props.ind + 1}`}
                     </Col>
                     <Col>
-                        <Button variant="danger" onClick={() => {
-                        
-                            const tmpSemesterCourses = [...props.semesterCourses];
-
-                            let theSemester = tmpSemesterCourses[0];
-
-                            let ind1 = 0;
-
-                            for(let i = 0; i < tmpSemesterCourses.length; i++){
-
-                                if(tmpSemesterCourses[i].semesternum === props.ind+1){
-                                    // found semester
-                                    ind1 = i;
-                                    theSemester = tmpSemesterCourses.splice(i,1)[0];
-                                    break;
-                                }
-
-                            }
-
-                            theSemester.courses = [];
-                            theSemester.courseSetter([]);
-
-                            tmpSemesterCourses.splice(ind1,0,theSemester);
-
-                            props.setSemesterCourses([...tmpSemesterCourses]);
-                        }}></Button>
+                        <Button onClick={semesterDeleteFunc} variant="danger" />
                     </Col>
                     <Col>
                         <Badge>
                             {`Credits: ${credits}`}
                         </Badge>
                     </Col>
-                    
                 </Accordion.Header>
                 <Accordion.Body>
                     <Col key={`semester-table-col-${props.ind}`}>
-                        <Droppable droppableId={`semester-table-${props.ind+1}`}>
+                        <Droppable droppableId={`semester-table-${props.ind + 1}`}>
                             {(prov: DroppableProvided) =>
+                                (
                                 <Table key={`semester-table-table-${props.ind}`}>
                                     <thead>
                                         <tr>
-                                            <td>{`${getSemesterStr(props.ind+1)}`}</td>
+                                            <td>{`${getSemesterStr(props.ind + 1)}`}</td>
                                         </tr>
                                     </thead>
                                     <tbody {...prov.droppableProps} ref={prov.innerRef}>
@@ -133,50 +149,28 @@ export const Semester = (props: { ind: number, semesterCourses: SemesterType[], 
                                             <td>
                                                 <ListGroup>
                                                     {
-                                                        courses.map((e, i) =>
-                                                            <>
-                                                                <ListGroup.Item key={i}>
+                                                        courses.map((elem: CourseType, index: number) =>
+                                                            (
+                                                                <ListGroup.Item key={elem}>
                                                                     <Row>
                                                                         <Col>
-                                                                            <Course name={`${e.name}-${e.section}`} ind={i}/>
+                                                                            <Course ind={index} name={`${elem.name}-${elem.section}`} />
                                                                         </Col>
-                                                                        <Col xs lg="1">
-                                                                            <Button variant="danger" onClick={() => {
-                                                                                const tmpCourses: CourseType[] = [...courses];
-                                                                                for(let i = 0; i < tmpCourses.length; i++){
-                                                                                    const theCourse: CourseType = tmpCourses[i];
-                                                                                    if(theCourse.name === e.name){
-                                                                                        // found course
-                                                                                        tmpCourses.splice(i,1);
-                                                                                    }
-                                                                                }
-                                                                                setCourses([...tmpCourses]);
-                                                                                const tmpSemesters: SemesterType[] = props.semesterCourses;
-                                                                                let tmpSemester: SemesterType = tmpSemesters[0];
-                                                                                for(let i = 0; i < tmpSemesters.length; i++){
-                                                                                    tmpSemester = tmpSemesters[i];
-                                                                                    if(tmpSemester.semesternum === props.ind+1){
-                                                                                        // found semester
-                                                                                        tmpSemester = tmpSemesters.splice(i,1)[0];
-                                                                                        tmpSemester.courses = [...tmpCourses];
-                                                                                        tmpSemester.courseSetter([...tmpCourses]);
-                                                                                        tmpSemesters.splice(i,0,tmpSemester);
-                                                                                        props.setSemesterCourses([...tmpSemesters]);
-                                                                                        return e.name;
-                                                                                    }
-                                                                                }
-
-                                                                                return e.name;
-                                                                            }}></Button>
-                                                                            <Button variant="warning" onClick={()=>{
+                                                                        <Col lg="1" xs>
+                                                                            <Button
+                                                                            onClick={courseDeleteFunc(elem)}
+                                                                            variant="danger"
+                                                                            />
+                                                                            <Button
+                                                                                onClick={() => {
                                                                                 setDisplay(!display);
-                                                                            }}>
-                                                                            </Button>
-                                                                            {display && <EditCourse display={display} setDisplay={setDisplay} course={e} setCourses={setCourses} semesterNumber={props.ind+1} semesterCourses={props.semesterCourses} setSemesterCourses={props.setSemesterCourses} ></EditCourse>}
+                                                                            }} variant="warning"
+                                                                            />
+                                                                            {display && <EditCourse course={elem} display={display} semesterCourses={props.semesterCourses} semesterNumber={props.ind + 1} setCourses={setCourses} setDisplay={setDisplay} setSemesterCourses={props.setSemesterCourses} />}
                                                                         </Col>
                                                                     </Row>
                                                                 </ListGroup.Item>
-                                                            </>
+                                                            ),
                                                         )
                                                     }
                                                 </ListGroup>
@@ -184,6 +178,7 @@ export const Semester = (props: { ind: number, semesterCourses: SemesterType[], 
                                         </tr>
                                     </tbody>
                                 </Table>
+                                )
                             }
                         </Droppable>
                     </Col>
