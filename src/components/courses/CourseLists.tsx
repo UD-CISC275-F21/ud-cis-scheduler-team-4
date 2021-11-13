7158e3bb523b7ecd87b01fe7f3c6e6bff12f6da2
Cameron Thacker <cthacker@udel.edu>
2021-11-13 08:41:40 -0500

    Trimmed down the drop logic for concentration to concentration to 6 lines of code

--- src/components/courses/CourseLists.tsx
+++
@@ -1,6 +0,0 @@
-import { Accordion } from "react-bootstrap";
-import CONCENTRATIONS from "../../json/concentrations.json";
-import { CourseContainer } from "./CourseContainer";
-import { ConcentrationContainerType } from "../../interfaces/concentrationcontainer";
-import React, { useState, useEffect } from "react";
-import { Course as CourseType } from "../../interfaces/course";
--- src/components/util/DropLogic.tsx
+++ src/components/util/DropLogic.tsx
@@ -5,6 +5,7 @@ import { ConcentrationContainerType } from "../../interfaces/concentrationcontai
 import { Course as CourseType } from "../../interfaces/course";
 
 export const concentrationToSemester = (
+    result: DropResult,
     concContainers: ConcentrationContainerType[],
     concContainer: ConcentrationContainerType,
     spliceInd: number,
@@ -16,6 +17,47 @@ export const concentrationToSemester = (
         const tmpConcentrationCourses = concContainer.courses;
         return -1;
 
+};
+
+export const semesterToConcentration = (
+    result: DropResult,
+    concContainers: ConcentrationContainerType[],
+    concContainer: ConcentrationContainerType,
+    spliceInd: number,
+    setConcContainers: React.Dispatch<React.SetStateAction<ConcentrationContainerType[]>>,
+    semesterCourses: SemesterType[],
+    setSemesterCourses: React.Dispatch<React.SetStateAction<SemesterType[]>>,
+    ) => {
+        console.log("doing semester to concentration");
+};
+
+export const concentrationToConcentration = (
+    result: DropResult,
+    concContainers: ConcentrationContainerType[],
+    concContainer: ConcentrationContainerType,
+    spliceInd: number,
+    courseSpliceInd: number,
+    dropInd: number,
+    setConcContainers: React.Dispatch<React.SetStateAction<ConcentrationContainerType[]>>,
+    ) => {
+        const tmpConcContainerCourse = concContainer.courses.splice(courseSpliceInd,1)[0];
+        concContainer.courses.splice(dropInd,0,tmpConcContainerCourse);
+        concContainer.setCourses([...concContainer.courses]);
+        concContainers.splice(spliceInd,0,concContainer);
+        setConcContainers(concContainers);
+        return 1;
+};
+
+export const semesterToSemester = (
+    result: DropResult,
+    concContainers: ConcentrationContainerType[],
+    concContainer: ConcentrationContainerType,
+    spliceInd: number,
+    setConcContainers: React.Dispatch<React.SetStateAction<ConcentrationContainerType[]>>,
+    semesterCourses: SemesterType[],
+    setSemesterCourses: React.Dispatch<React.SetStateAction<SemesterType[]>>,
+    ) => {
+        console.log("semester to semester");
 };
 
 export const onDragEndLogic = (result: DropResult,
@@ -34,19 +76,28 @@ export const onDragEndLogic = (result: DropResult,
    const destinationId = result.destination.droppableId;
    const sourceIdSemester = sourceId.includes("semester-table");
    const destIdSemester = destinationId.includes("semester-table");
+   const sourceIndex = result.source.index;
+   const dropIndex = result.destination.index;
    if (sourceId === destinationId) {
         // check if both are semester table, if not then conc container
-        
+        if(sourceIdSemester){
+            // semestertable --> semestertable
+        } else{
+            // concentration --> concentration
+            const tmpContainer: ConcentrationContainerType[] = [...concentrationContainers];
+            let ind1 = tmpContainer.findIndex(elem => elem.name === destinationId);
+            ind1 = (ind1 !== -1) ? concentrationToConcentration(result, concentrationContainers, concentrationContainers.splice(ind1, 1)[0], ind1, sourceIndex, dropIndex, setConcentrationContainers) : -1;
+        }
     } else {
        // dropId != destinationId
         if (sourceIdSemester) { // semester --> concentration
         } else {
             // concentration --> semester
             const tmpContainer: ConcentrationContainerType[] = [...concentrationContainers];
-            const ind1 = tmpContainer.findIndex(elem => elem.name === sourceId);
-            ind1 !== -1 ? concentrationToSemester(concentrationContainers, concentrationContainers[ind1], result.source.index, setConcentrationContainers, semesterCourses, setSemesterCourses) : -1;
+            let ind1 = tmpContainer.findIndex(elem => elem.name === sourceId);
+            ind1 = (ind1 !== -1) ? concentrationToSemester(result, concentrationContainers, concentrationContainers[ind1], result.source.index, setConcentrationContainers, semesterCourses, setSemesterCourses) : -1;
         }
-   }
+    }
 
 
 
