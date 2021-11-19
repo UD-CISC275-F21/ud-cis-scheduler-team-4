1314e9fd7fafff68e055735b4ea5917738585076
lukehalko <halko@udel.edu>
2021-11-18 21:58:22 -0500

    updated document title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootswatch/dist/lux/bootstrap.min.css";
 import { MainPage } from "./components/MainPage";
 import "./App.css";
 
-export const App = (): JSX.Element =>
-    <div className="App">
-        <MainPage />
-    </div>;
+export const App = (): JSX.Element => {
+    useEffect(() => {
+        document.title = "UDCIS Course Scheduling Tool";
+    }, []);
+    return (
+        <div className="App">
+            <MainPage />
+        </div>
+    );
+};
 export default App;
+    
