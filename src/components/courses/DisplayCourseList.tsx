c0f4bbbe1db0736750f03a7fa2f9355baae91b17
Cameron Thacker <cthacker@udel.edu>
2021-11-28 22:51:09 -0500

    Removed unnecessary imports

--- src/components/courses/DisplayCourseList.tsx
+++ src/components/courses/DisplayCourseList.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 import { Concentration } from "../../interfaces/concentration";
-import { AIConc } from "./concentrations/AIConc";
-import { BioConc } from "./concentrations/BioConc";
-import { DataScienceConc } from "./concentrations/DataScienceConc";
-import { HPCConc } from "./concentrations/HPCConc";
-import { NetworksConc } from "./concentrations/NetworksConc";
-import { SecurityConc } from "./concentrations/SecurityConc";
-import { TheoryConc } from "./concentrations/TheoryConc";
-import CONCENTRATIONS from "../../json/concentrations.json";
-import COURSES from "../../json/courses.json";
-import { Course as CourseType } from "../../interfaces/course";
 import { ConcentrationContainerType } from "../../interfaces/concentrationcontainer";
 import { DisplayCourseListMap } from "./DisplayCourseListHelperFunctions/DisplayCourseListMap";
 
