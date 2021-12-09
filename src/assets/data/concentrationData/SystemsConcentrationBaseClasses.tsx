68139520e4ee30b584aad1e1048a71b68176c735
Cameron Thacker <cthacker@udel.edu>
2021-12-09 18:22:23 -0500

    Cleaned out all unecessary files

--- src/assets/data/concentrationData/SystemsConcentrationBaseClasses.tsx
+++
@@ -1,4 +0,0 @@
-import CONCENTRATIONS from "../../../json/concentrations.json";
-import { Concentration } from "../../../interfaces/concentration";
-
-export const SystemsConcentrationBaseClasses = CONCENTRATIONS[5] as Concentration;
