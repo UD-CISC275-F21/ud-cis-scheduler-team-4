0c7770adb469d852826fe8d07de737e8538fd6eb
Cameron Thacker <cthacker@udel.edu>
2021-12-09 17:59:04 -0500

    Fixed all warnings

--- src/components/util/SaveDataFunctions/UpdateMainPageStateWithSaveData.tsx
+++ src/components/util/SaveDataFunctions/UpdateMainPageStateWithSaveData.tsx
@@ -1,6 +1,5 @@
 import { SavedProgress } from "../../../interfaces/savedprogress";
 import { Semester } from "../../../interfaces/semester";
-import { Concentration } from "../../../interfaces/concentration";
 
 export const UpdateMainPageStateWithSaveData = (
     saveData: SavedProgress,
