01534dc83adf95898d9787bf61d4eda4d4953786
Cameron Thacker <cthacker@udel.edu>
2021-12-12 15:13:45 -0500

    Added import tweak for loading in csv file with created courses

--- src/components/courses/DisplayCourseListHelperFunctions/StringsToCourses.tsx
+++ src/components/courses/DisplayCourseListHelperFunctions/StringsToCourses.tsx
@@ -7,5 +7,26 @@ export const StringsToCourses = (stringCourses: string[]): Course[] => {
      */
     const allCourses = COURSES as Course[];
     const courseList: Course[] = stringCourses.map(elem => [...allCourses.filter(i => i.name === elem)]).flat(2);
+    if (courseList.length != stringCourses.length) {
+        // one or all of the courses were not registered in
+        const names = courseList.map((eachCourse) => eachCourse.name);
+        for(const eachName of stringCourses) {
+            if (!names.includes(eachName)) {
+                // found course not read in
+                courseList.push({
+                    name: eachName,
+                    title: "Update Title",
+                    description: "Not yet updated",
+                    section: 10,
+                    prereqs: [],
+                    coreqs: [],
+                    credits: 3,
+                    lab: false,
+                    fromIndex: 0,
+                    fromContainerIndex: 0
+                });
+            }
+        }
+    }
     return courseList;
 };
